f22c27a95da9ba88a1e979c7a501b7025c54f4f9
shanth <shanth@shanth-Lenovo-ideapad-330-15IKB>
2022-11-03 13:40:41 +0530

    frame

--- src/test/java/scripts/Test0.java
+++ src/test/java/scripts/Test0.java
@@ -1,6 +1,7 @@
 package scripts;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
--- src/test/java/scripts/Test2.java
+++ src/test/java/scripts/Test2.java
@@ -1,12 +1,13 @@
 package scripts;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Test2 {
 @Test
 public void tc3(){
-	Reporter.log("hello java",true);
+	Reporter.log("hello selnium",true);
 }
 }
