b12f138e52cb569793faf3ef07e68659fa863c5e
shanth <shanth@shanth-Lenovo-ideapad-330-15IKB>
2022-11-03 15:13:18 +0530

    new test

--- src/test/java/scripts/Test1.java
+++ src/test/java/scripts/Test1.java
@@ -3,6 +3,7 @@ package scripts;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
